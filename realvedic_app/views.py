8128f8cea40985b7c44451b569f67c98d66659aa
Simrankaur0520 <122337420+Simrankaur0520@users.noreply.github.com>
2023-01-23 01:21:46 -0800

    single_product_view

--- realvedic_app/views.py
+++ realvedic_app/views.py
@@ -33,63 +33,6 @@ from realvedic_app.models import Product_data,categoryy,images_and_banners,blogs
 #----------------------------extra---------------------------------------------------
 import simplejson as json
 
-def data_input_pandas():
-    #reading and writing product data from csv file 
-    '''df=pd.read_csv('product_view.csv')
-    df =df.fillna(0)
-    res=df.to_dict(orient='records')
-    #dff=df.apply(lambda x: x.split(","),axis=0)'''
-    '''for i in range(len(res)):
-
-        Product_name =res[i]["name"]
-        category =res[i]["category"]
-        prices =res[i]["price"]
-        Sizes =res[i]["sizes"]
-        benefits =res[i]["benefits"]
-        ingredients=res[i]["ingredients"]
-        how_to_use=res[i]["how_to_use"]
-        how_we_make_it =res[i]["how_we_make_it"]
-        nutrition=res[i]["nutrition_info"]
-        Status = "Inactive"
-        sibling_product=res[i]["sibling_product"]
-        HSN=res[i]["HSN "]
-        SKU=res[i]["SKU"]
-        print(category)
-     
-
-        data=Product_data(
-            Product_name =Product_name,
-            category =category,
-            prices =prices,
-            Sizes = Sizes,
-            benefits =benefits,
-            ingredients= ingredients,
-            how_to_use=how_to_use,
-            how_we_make_it =how_we_make_it,
-            nutrition=nutrition,
-            Status = Status,
-            sibling_product=sibling_product,
-            HSN=HSN,
-            SKU=SKU       
-            )
-        data.save()'''
-        #reading and writing data into databae for category details from csv file
-        # 
-        #   '''title="banner"+str(i)
-    '''image="banner_image"+str(i)
-        print(title,image)
-        data=images_and_banners(
-            title=title,
-            image=image
-        )
-        data.save()'''
-    ''' titlee="Make Best Dosa with us!"
-            imagee="https://youtu.be/EkJC0GgY5wk"
-            data=images_and_banners(
-            title=titlee,
-            image=imagee
-            ''' 
-
 
 #Putting data into database
 @api_view(['GET'])
@@ -220,12 +163,154 @@ def write_data(request,format=None):
     res['blog']=blog_obj
 
     return Response(res)
-    
+
 
 @api_view(['GET'])
-def category_tab_view(request,format=None):
-        pass
-        '''df=pd.read_csv('realvedic_category.csv')
+def single_product_view(request,format=None):
+    obj=Product_data.objects.filter(id=10).values()
+    #-------------------Dictionaries and list initialisation 
+    #--------------Mock description-------------------------
+    desc=str("A ready dosa mix without going through the hassle of soaking, grinding, and preparing the batter. Just add water and salt, rest for few minutes, and start making tasty and healthy dosas. We have combined 80 sprouted green gram with nutritious moringa leaves and spices for a power-packed quick meal any time of the day.")
+    res={}
+   
+    benefits={}
+    pack_sizes=[]
+    #-----------------assigning product_details to the response
+    for i in obj:
+        weight=i["Sizes"].split("|")
+        prices=i["prices"].split("|")
+        for j in range(len(weight)):
+            weight_price={
+                'weight':weight[j],
+                'price':prices[j],
+                'Offer_price':int(prices[j])+5
+            }
+            pack_sizes.append(weight_price)
+        prod_details={
+            'id':i["id"],
+            "title":i["Product_name"],
+            'description':desc,
+            "original_price":i["prices"].split("|")[0],
+            "offer_price":int(i["prices"].split("|")[0])+5,
+            'single_image':i["image"],
+            'images':["",""],
+            'pack_sizes':pack_sizes}
+            
+        benefits={
+                'title':"benefits",
+                'description':i["benefits"]
+            }
+        ingredients={
+                'title':"ingredients",
+                'description':i["ingredients"]
+            }
+        how_to_use={
+                'title':"how_to_use",
+                'description':i["how_to_use"]
+            }
+        how_we_make_it={
+                'title':"how_we_make_it",
+                'description':i["how_we_make_it"]
+            }
+        nutrition={
+                "title": "Nutritional Info per 100g (Approx)*",
+                "values":
+                [
+                {
+                    "title": "Total Fat",
+                    "value": i['nutrition'].split("|")[0]
+                },
+                {
+                    "title": "Protien",
+                    "value": i['nutrition'].split("|")[1]
+                },
+                {
+                    "title": "Carbohydrate   ",
+                    "value": i['nutrition'].split("|")[2]
+                },
+                {
+                    "title": "Energy",
+                    "value": i['nutrition'].split("|")[3]
+                }
+                ]
+                }
+                    
+   
+        res['product_details']=prod_details
+        res['benefits']=benefits
+        res['ingredients']=ingredients
+        res['how_to_use']=how_to_use
+        res['how_we_make_it']=how_we_make_it 
+        res['nutrition']=nutrition
+
+            
+
+
+
+        
+    return Response(res)
+    
+
+
+       
+
+def data_input_pandas():
+    #reading and writing product data from csv file 
+    '''df=pd.read_csv('product_view.csv')
+    df =df.fillna(0)
+    res=df.to_dict(orient='records')
+    #dff=df.apply(lambda x: x.split(","),axis=0)'''
+    '''for i in range(len(res)):
+
+        Product_name =res[i]["name"]
+        category =res[i]["category"]
+        prices =res[i]["price"]
+        Sizes =res[i]["sizes"]
+        benefits =res[i]["benefits"]
+        ingredients=res[i]["ingredients"]
+        how_to_use=res[i]["how_to_use"]
+        how_we_make_it =res[i]["how_we_make_it"]
+        nutrition=res[i]["nutrition_info"]
+        Status = "Inactive"
+        sibling_product=res[i]["sibling_product"]
+        HSN=res[i]["HSN "]
+        SKU=res[i]["SKU"]
+        print(category)
+     
+
+        data=Product_data(
+            Product_name =Product_name,
+            category =category,
+            prices =prices,
+            Sizes = Sizes,
+            benefits =benefits,
+            ingredients= ingredients,
+            how_to_use=how_to_use,
+            how_we_make_it =how_we_make_it,
+            nutrition=nutrition,
+            Status = Status,
+            sibling_product=sibling_product,
+            HSN=HSN,
+            SKU=SKU       
+            )
+        data.save()'''
+        #reading and writing data into databae for category details from csv file
+        # 
+        #   '''title="banner"+str(i)
+    '''image="banner_image"+str(i)
+        print(title,image)
+        data=images_and_banners(
+            title=title,
+            image=image
+        )
+        data.save()'''
+    ''' titlee="Make Best Dosa with us!"
+            imagee="https://youtu.be/EkJC0GgY5wk"
+            data=images_and_banners(
+            title=titlee,
+            image=imagee
+            ''' 
+'''df=pd.read_csv('realvedic_category.csv')
         df =df.fillna(0)
         res=df.to_dict(orient='records')
         for i in range(len(res)):
@@ -244,7 +329,7 @@ def category_tab_view(request,format=None):
 
     
        
-        '''res={}
+'''res={}
         tab=[]
         prod_tab={
             "title": obj.category,
@@ -257,41 +342,5 @@ def category_tab_view(request,format=None):
         res="Something went wrong, Try again"
         return Response(res)
 
-def banner_view(request,fomat=None):
-    res={}
-    banner=[]
-    try:
-        #obj
-        bann={
-            'image':obj.image,
-            'title':obj.category
-        }
-        
-        banner.append(bann)
-        res['banner']:banner
-        return Response(res)
-    except:
-        res="Something Wents wrong"
-        return Response(res)
 
-def top_sellers(request,format=None):
-    res={}
-    top_selling_products=[]
-    try:
-        #obj=Product_data.objects.values('category','image','catrgory_colour')
-        prod={
-            "image": obj.image,
-            "title": obj.name,
-            "weight": obj.weight,
-            "price": obj.prices,
-    }
-        top_selling_products.append(prod)
-        res['products']:top_selling_products
-        return Response(res)
-    except:
-        res="Something Wents wrong"
-        return Response(res)
-
-
-def new():
-    pass'''
+'''
